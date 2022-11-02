5b65f7b2a8b8ec69af5f7099d84f9f28e64b7667
Jock Dalby <jock.dalby@citrusad.com>
2022-11-03 08:05:57 +1000

    add useState hook to SearchParams component

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,11 +1,13 @@
+import { useState } from 'react';
+
 const SearchParams = () => {
-  const location = "Gold Coast, QLD";
+  const [location, setLocation] = useState("Gold Coast, QLD");
   return (
     <div className="search-params">
       <form>
         <label htmlFor="location">
           Location
-          <input id="location" value={location} placeholder="Location" />
+          <input id="location" value={location} placeholder="Location" onChange={(e) => setLocation(e.target.value)}/>
         </label>
         <button>Submit</button>
       </form>
