1b8b29f49c6f44bf7b8cec7210c2eb3e11ebc6b4
Jock Dalby <jock.dalby@citrusad.com>
2022-11-03 08:18:32 +1000

    add animal and breed dropdowns and useState hooks to SearchParams component

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,18 +1,70 @@
 import { useState } from 'react';
 
+const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
+const BREEDS = ["bird", "cat", "dog", "rabbit", "reptile"];
+
 const SearchParams = () => {
   const [location, setLocation] = useState("Gold Coast, QLD");
+  const [animal, setAnimal] = useState("");
+  const [breed, setBreed] = useState("");
   return (
     <div className="search-params">
       <form>
         <label htmlFor="location">
           Location
-          <input id="location" value={location} placeholder="Location" onChange={(e) => setLocation(e.target.value)}/>
+          <input
+            id="location"
+            value={location}
+            placeholder="Location"
+            onChange={(e) => setLocation(e.target.value)}
+          />
+        </label>
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            value={animal}
+            onChange={(e) => {
+              setAnimal(e.target.value);
+              setBreed("");
+            }}
+            onBlur={(e) => {
+              setAnimal(e.target.value);
+              setBreed("");
+            }}
+          >
+            <option />
+            {ANIMALS.map((a) => (
+              <option key={a} value={a}>
+                {a}
+              </option>
+            ))}
+          </select>
+        </label>
+        <label htmlFor="breed">
+          Breed
+          <select
+            id="breed"
+            value={breed}
+            onChange={(e) => {
+              setBreed(e.target.value);
+            }}
+            onBlur={(e) => {
+              setBreed(e.target.value);
+            }}
+          >
+            <option />
+            {BREEDS.map((b) => (
+              <option key={b} value={b}>
+                {b}
+              </option>
+            ))}
+          </select>
         </label>
         <button>Submit</button>
       </form>
     </div>
-  )
+  );
 }
 
 export default SearchParams;
