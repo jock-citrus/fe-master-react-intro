99febfed767a9608709b3fabf3e1cf15d07be294
Jock Dalby <jock.dalby@citrusad.com>
2022-11-07 08:49:44 +1000

    install react-router-dom and add BrowserRouter and routes to app.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
-import React from "react";
+import React, { StrictMode } from "react";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import ReactDOM from "react-dom";
 import SearchParams from './SearchParams'
+import Details from './Details';
 
 const App = () => {
   return (
-    <div>
-      <h1>Adopt Me!</h1>
-      <SearchParams />
-      {/* <Pet name="Luna" animal="Dog" breed="Havanese" />
-      <Pet name="Pepper" animal="Bird" breed="Cockatiel" />
-      <Pet name="Doink" animal="Cat" breed="Mix" /> */}
-    </div>
+    <StrictMode>
+      <BrowserRouter>
+        <h1>Adopt Me!</h1>
+        <Routes>
+          <Route path="/details/:id" element={<Details />} />
+          <Route path="/" element={<SearchParams />} />
+        </Routes>
+      </BrowserRouter>
+    </StrictMode>
   );
 };
 
