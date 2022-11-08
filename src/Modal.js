918f9dd81bb4415772fc5c03743eb0f76ca994fd
Jock Dalby <jock.dalby@citrusad.com>
2022-11-09 08:50:27 +1000

    create Modal component

---
+++ src/Modal.js
@@ -0,0 +1,20 @@
+import { useEffect, useRef } from "react";
+import { createPortal } from "react-dom"; 
+
+const Modal = ({ children }) => {
+  const elRef = useRef(null);
+  if(!elRef.current) {
+    elRef.current = document.createElement("div");
+  }
+
+  useEffect(() => {
+    const modalRoot = document.getElementById("modal");
+    modalRoot.appendChild(elRef.current);
+
+    // Whatever return in useEffect is for clean up, to
+    // prevent memory leaks.
+    return () => modalRoot.removeChild(elRef.current)
+  }, [])
+
+  return createPortal(<div>{children}</div>, elRef.current)
+}
